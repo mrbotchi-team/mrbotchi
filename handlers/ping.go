e2bbf468ea501505189f577fc65440583e21b23f
silverscat_3 <silverscat_3@mail.sc3.fun>
2019-07-05 17:05:35 +0900

    content-typeを返すようにした

--- handlers/ping.go
+++ handlers/ping.go
@@ -20,6 +20,9 @@ type (
 func (h PingHandler) Get(w http.ResponseWriter, r *http.Request) {
 	response := pingResponse{Message: "Guten Morgen!! I'm Mr Botchi! maybe backend!", Version: h.app.Version}
 
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
 	if err := json.NewEncoder(w).Encode(response); nil != err {
 		error.NewInternalServerError().Response(w, r)
 	}
